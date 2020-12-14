b33e9fbd31b1aadab8c4914b26d021110a0a5c6d
sciencemj <sciencemj.park@gmail.com>
2020-12-14 18:07:39 +0900

    commit

--- src/rpg/Main.java
+++ src/rpg/Main.java
@@ -1,13 +1,13 @@
 package rpg;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
-import org.bukkit.Location;
+import com.mysql.fabric.xmlrpc.base.Array;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.block.CommandBlock;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.text.translate.EntityArrays;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -20,173 +20,96 @@ import java.io.*;
 import java.util.*;
 
 public class Main extends JavaPlugin{
-    public static HashMap<Player, Integer> levels = new HashMap<Player, Integer>();
-    public static Queue<Character> cmds = new LinkedList<Character>();
-    public static ArrayList<MagicI> magicArray = new ArrayList<MagicI>();
-    public static Main pluginm;
-    ArrayList<String> except = new ArrayList<String>();
-    Player target;
+    public static Block[][] mines;
+    public static ArrayList<ArmorStand> stands = new ArrayList<ArmorStand>();
+    public static int[][] map;
+    public static int size;
+    public static int bombs;
     @Override
     public void onEnable() {
-        pluginm = this;
-        Bukkit.getPluginCommand("command").setExecutor(this);
-        Bukkit.getPluginCommand("rmb").setExecutor(this);
-        Bukkit.getPluginCommand("magic").setExecutor(this);
-        Bukkit.getPluginManager().registerEvents(new MainEvent(),this);
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-                if (!cmds.isEmpty()){
-                    Location l = target.getLocation();
-                    switch (cmds.poll()){
-                        case 'w':
-                            l.setPitch(0f);
-                            target.teleport(l);
-                            target.setVelocity(target.getLocation().getDirection().setY(0).multiply(0.5f));
-                            break;
-                        case 's':
-                            l.setPitch(0f);
-                            target.teleport(l);
-                            target.setVelocity(target.getLocation().getDirection().setY(0).multiply(-0.5f));
-                            break;
-                        case 'a':
-                            l.setPitch(0f);
-                            l.setYaw(l.getYaw() - 90);
-                            target.teleport(l);
-                            target.setVelocity(l.getDirection().setY(0).multiply(0.5f));
-                            break;
-                        case 'd':
-                            l.setPitch(0f);
-                            l.setYaw(l.getYaw() + 90);
-                            target.teleport(l);
-                            target.setVelocity(l.getDirection().setY(0).multiply(0.5f));
-                            break;
-                        case 'j':
-                            target.setVelocity(new Vector(0,0.5,0));
-                            break;
-                        default:
-                            target.sendMessage("wrong command");
-                    }
-                }
-            }
-        }, 0L, 5L);
-        String line = null;
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader("save.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while(true){
-            try {
-                line = reader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (line == null)
-                break;
-            String[] str = line.split(" ");
-            switch (str[2]){
-                case "0":
-                    Color color = Color.fromRGB(Integer.parseInt(str[4]),Integer.parseInt(str[5]),Integer.parseInt(str[6]));
-                    magicArray.add(new MagicI(str[0],str[1],str[2],Integer.parseInt(str[3]),color,Float.parseFloat(str[7])));
-            }
-        }
+        Bukkit.getPluginCommand("minestart").setExecutor(this);
+        Bukkit.getPluginCommand("minestop").setExecutor(this);
+        Bukkit.getServer().getPluginManager().registerEvents(new MainEvent(),this);
+
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equals("command")){
-            target = Bukkit.getPlayer(args[0]);
-            for (char ch : args[1].toCharArray()) {
-                cmds.add(ch);
-            }
-        }else if(command.getName().equals("rmb")){
-            if (args[0].equals("except")){
-                sender.sendMessage("ex");
-                except.add(args[1]);
-            }else {
-                Location l = new Location(Bukkit.getWorlds().get(0), Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
-                Random r = new Random();
-                boolean bool = true;
-                int i = 0;
-                EntityType type = null;
-                while (bool) {
-                    type = EntityType.values()[r.nextInt(EntityType.values().length)];
-                    bool = false;
-                    for (String s : except){
-                        if (type.name().equals(s))
-                            bool = true;
-                    }
-                    i++;
-                    if (i >= EntityType.values().length){
-                        break;
+        if (command.getName().equals("minestart") && args.length == 2) {
+            if (sender instanceof Player){
+                Location l = ((Player) sender).getLocation();
+                Location p = l.clone();
+                size = Integer.parseInt(args[0]);
+                bombs = Integer.parseInt(args[1]);
+                mines = new Block[size][size];
+                for (int i = 0;i < size; i++){
+                    for (int j = 0; j < size;j++) {
+                        Block b = p.add(i,0,j).getBlock();
+                        b.setType(Material.STONE);
+                        mines[i][j] = b;
+                        p = l.clone();
                     }
                 }
-                Objects.requireNonNull(l.getWorld()).spawnEntity(l, type);
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    p.sendTitle(ChatColor.RED + type.name(), "", 20, 0, 0);
-                }
-            }
-        }else if (command.getName().equals("magic")){
-            if (args[0].equals("make")) {
-                switch (args[2]) {
-                    case "0":
-                        if (args.length == 8){
-                            MagicI magic = new MagicI(sender.getName(), args[1], args[2], Integer.parseInt(args[3]),
-                                    Color.fromRGB(Integer.parseInt(args[4]), Integer.parseInt(args[5]), Integer.parseInt(args[6])),
-                                    Float.parseFloat(args[7]));
-
-                            sender.sendMessage(magic.maker);
-                            magicArray.add(magic);
-                        }else {
-                            sender.sendMessage("/magic make " + ChatColor.YELLOW + "<name> <type> <range> <red> <green> <blue> <damage>");
-                        }
-                        break;
-                }
-            }else if (args[0].equals("item")){
-                if (sender instanceof Player) {
-                    Player p = Bukkit.getPlayer(args[1]);
-                    ItemMeta meta = p.getInventory().getItemInMainHand().getItemMeta();
-                    MagicI magic = null;
-                    for (MagicI m:magicArray)
-                        if (m.name.equals(args[2])) magic = m;
-                    if (meta.hasLore()) {
-                        List<String> list = meta.getLore();
-                        list.add(magic.name);
-                        meta.setLore(list);
-                        p.getInventory().getItemInMainHand().setItemMeta(meta);
-                    } else {
-                        List<String> list = new ArrayList<String>();
-                        list.add(magic.name);
-                        meta.setLore(list);
-                        p.getInventory().getItemInMainHand().setItemMeta(meta);
+                /*map = mapMaker(size, bombs);
+                String s = "";
+                for (int i = 0; i < size;i++) {
+                    for (int j = 0; j < size;j++) {
+                        s = s + String.valueOf(map[i][j]) + " ";
                     }
-                }
-            }else if (args[0].equals("list")){
-                for (MagicI magic : magicArray){
-                    sender.sendMessage(magic.name);
-                }
+                    sender.sendMessage(s);
+                    s="";
+                }*/
             }
+        }else if (command.getName().equals("minestop")){
+            init();
         }
-        return false;
+        return true;
     }
 
-    void saveData() throws IOException {
-        FileWriter writer = new FileWriter("save.txt");
-        for (MagicI magic : magicArray){
-            writer.write(magic.getInfo()+"\r\n");
+    public static int[][] mapMaker(int size, int bombs, int x, int y){
+        int bsize = size*size;
+        if (bsize >= bombs) {
+            int[][] map = new int[size][size];
+            map[x][y] = 100;
+            int loc;
+            int loc2;
+            for (int i = 0; i < bombs; i++) {
+                while (true) {
+                    Random r = new Random();
+                    loc = r.nextInt(size);
+                    loc2 = r.nextInt(size);
+                    if (map[loc][loc2] != -1 && map[loc][loc2] < 100) {
+                        map[loc][loc2] = -1;
+                        for (int a = -1;a < 2;a++){
+                            for (int b = -1;b < 2;b++){
+                                if (((loc + a) >= 0 && (loc2 + b) >= 0) && (((loc + a) < size) && ((loc2 + b) < size))){
+                                    if (map[loc+a][loc2+b] != -1){
+                                        map[loc+a][loc2+b]++;
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            map[x][y] = map[x][y] - 100;
+            return map;
+        }else {
+            return null;
         }
-        writer.close();
+    }
 
+    public static void init(){
+        map = null;
+        for (ArmorStand stand:stands) {
+            stand.setHealth(0D);
+        }
+        mines = null;
     }
 
     @Override
     public void onDisable() {
-        try {
-            saveData();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        init();
     }
 }
--- src/rpg/MainEvent.java
+++ src/rpg/MainEvent.java
@@ -1,5 +1,8 @@
 package rpg;
 
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,44 +12,85 @@ import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 public class MainEvent implements Listener {
-    /*@EventHandler
-    public void event(PlayerExpChangeEvent e){
-        Player p = e.getPlayer();
-        p.sendMessage(p.getTotalExperience() +  "s");
-        p.sendMessage(p.getTotalExperience() + e.getAmount() +  "t");
-        if (p.getTotalExperience() + e.getAmount() >= 7){
-            p.sendMessage("over:" + (e.getAmount() - (7 - p.getTotalExperience())));
-            e.setAmount( e.getAmount() - (7 - p.getTotalExperience()));
-            p.setTotalExperience(0);
-            p.setExp(0f);
-            p.setLevel(0);
-            if (Main.levels.containsKey(p)) {
-                Main.levels.replace(p, Main.levels.get(p) + 1);
-            }else {
-                Main.levels.put(p, 1);
-            }
-            p.sendMessage("레벨:" + Main.levels.get(p));
-        }
-
-    }*/
     @EventHandler
     public void PlayerMove(PlayerMoveEvent e){
         Player p = e.getPlayer();
     }
+
     @EventHandler
-    public  void playerEvent(PlayerInteractEvent e){
+    public void playerEvent(PlayerInteractEvent e){
         Player p = e.getPlayer();
-        if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
-            ItemStack item = p.getInventory().getItemInMainHand();
-            for (String s:item.getItemMeta().getLore()){
-                for (MagicI m: Main.magicArray){
-                    if (s.equals(m.name)){
-                        m.play(p);
+        if (e.getAction() == Action.LEFT_CLICK_BLOCK){
+            Block b = e.getClickedBlock();
+            for (int i = 0;i < Main.mines.length;i++){
+                for (int j = 0;j < Main.mines.length;j++){
+                    if (Main.mines[i][j].getLocation().equals(b.getLocation())){
+                        if (Main.map == null){
+                            Main.map = Main.mapMaker(Main.size,Main.bombs,i,j);
+                        }
+                        e.setCancelled(true);
+                        switch (Main.map[i][j]){
+                            case -1:
+                                b.setType(Material.TNT);
+                                p.sendTitle(ChatColor.RED + "FAILED", "", 60,20,20);
+                                p.getWorld().playSound(p.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 10,20);
+                                p.getWorld().spawnParticle(Particle.EXPLOSION_HUGE,p.getLocation(),2);
+                                Main.init();
+                                break;
+                            case 0:
+                                b.setType(Material.WHITE_CONCRETE);
+                                break;
+                            case 1:
+                                b.setType(Material.GREEN_CONCRETE);
+                                nameMaker(b,"1");
+                                break;
+                            case 2:
+                                b.setType(Material.BLUE_CONCRETE);
+                                nameMaker(b,"2");
+                                break;
+                            case 3:
+                                b.setType(Material.YELLOW_CONCRETE);
+                                nameMaker(b,"3");
+                                break;
+                            case 4:
+                                b.setType(Material.ORANGE_CONCRETE);
+                                nameMaker(b,"4");
+                                break;
+                            case 5:
+                                b.setType(Material.PINK_CONCRETE);
+                                nameMaker(b,"5");
+                                break;
+                            case 6:
+                                b.setType(Material.RED_CONCRETE);
+                                nameMaker(b,"6");
+                                break;
+                            case 7:
+                                b.setType(Material.BROWN_CONCRETE);
+                                nameMaker(b,"7");
+                                break;
+                            case 8:
+                                b.setType(Material.BLACK_CONCRETE);
+                                nameMaker(b,"8");
+                                break;
+                        }
                     }
                 }
             }
         }
+    }
+
+    public ArmorStand nameMaker(Block b,String s){
+        ArmorStand stand = b.getWorld().spawn(b.getLocation().add(0.5,-1,0.5), ArmorStand.class);
+
+        stand.setVisible(false);
+        stand.setGravity(false);
+        stand.setCustomNameVisible(true);
+        stand.setCustomName(s);
+        stand.setCollidable(false);
+        Main.stands.add(stand);
+        return stand;
     }
 }
